a4f04c047a586d068656f97201c8e73e559a22ea
n1trohz <alanaapta@gmail.com>
2024-04-01 20:07:42 -0700

    ini Commit kedua

--- app.py
+++ app.py
@@ -5,8 +5,21 @@ print()
 def hitungLuasSegitiga(a,t):
     return round(0.5 * a * t,2)
 
-alas = float(input("Input Alas Segitiga"))
-tinggi = float(input("Input Tinggi Segitiga"))
+alas = float(input("Input Alas Segitiga : "))
+tinggi = float(input("Input Tinggi Segitiga : "))
 
 print('Luas Segitiga = ', hitungLuasSegitiga(alas,tinggi))
 print()
+
+print("## PROGRAM PYTHON MENGHITUNG PANJANG PERSEGI ##")
+print("=============================================")
+print()
+
+def hitungPersegiPanjang(p,l):
+    return round(p * l,2)
+
+panjang = float(input('Input Panjang : '))
+lebar = float(input('Input Lebar : '))
+print('Luas Persegi Panjang = ',hitungPersegiPanjang(panjang,lebar))
+
+
